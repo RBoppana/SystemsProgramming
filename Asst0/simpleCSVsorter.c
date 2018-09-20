43401174d3d34d26a4834f62fa4f5bac365ed1de
Rahul S. Boppana <rsb172@ilab3.cs.rutgers.edu>
2018-09-20 16:46:03 -0400

    skeleton for c file and set sizes for h file

--- Asst0/simpleCSVsorter.c
+++ Asst0/simpleCSVsorter.c
@@ -6,4 +6,89 @@
 
 int main(int argc, char** argv){
 	
+	//Column Names
+	scanf("%s,", temp->color);
+	scanf("%s,", temp->director_name);
+	scanf("%d,", temp->num_critic_for_reviews);
+	scanf("%d,", temp->duration);
+	scanf("%d,", temp->director_facebook_likes);
+	scanf("%d,", temp->actor_3_facebook_likes);
+	scanf("%s,", temp->actor_2_name);
+	scanf("%d,", temp->actor_1_facebook_likes);
+	scanf("%d,", temp->gross);
+	scanf("%s,", temp->genres);
+	scanf("%s,", temp->actor_1_name);
+	scanf("%s,", temp->movie_title);
+	scanf("%d,", temp->num_voted_users);
+	scanf("%d,", temp->cast_total_facebook_likes);
+	scanf("%s,", temp->actor_3_name);
+	scanf("%d,", temp->facenumber_in_poster);
+	scanf("%s,", temp->plot_keywords);
+	scanf("%s,", temp->movie_imdb_link);
+	scanf("%d,", temp->num_user_for_reviews);
+	scanf("%s,", temp->language);
+	scanf("%s,", temp->country);
+	scanf("%s,", temp->content_rating);
+	scanf("%d,", temp->budget);
+	scanf("%d,", temp->title_year);
+	scanf("%d,", temp->actor_2_facebook_likes);
+	temp->double imdb_score);
+	temp->double aspect_ratio); //idk the letter for double
+	scanf("%d,", temp->int movie_facebook_likes);
+	scanf("/n");
+
+	while(/*there is more lines*/){
+		Listing* temp = (Listing*)malloc(sizeof(Listing)); //should i reallocate memory instead of mallocing
+		scanf("%s,", temp->color);
+		scanf("%s,", temp->director_name);
+		scanf("%d,", temp->num_critic_for_reviews);
+		scanf("%d,", temp->duration);
+		scanf("%d,", temp->director_facebook_likes);
+		scanf("%d,", temp->actor_3_facebook_likes);
+		scanf("%s,", temp->actor_2_name);
+		scanf("%d,", temp->actor_1_facebook_likes);
+		scanf("%d,", temp->gross);
+		scanf("%s,", temp->genres);
+		scanf("%s,", temp->actor_1_name);
+		scanf("%s,", temp->movie_title);
+		scanf("%d,", temp->num_voted_users);
+		scanf("%d,", temp->cast_total_facebook_likes);
+		scanf("%s,", temp->actor_3_name);
+		scanf("%d,", temp->facenumber_in_poster);
+		scanf("%s,", temp->plot_keywords);
+		scanf("%s,", temp->movie_imdb_link);
+		scanf("%d,", temp->num_user_for_reviews);
+		scanf("%s,", temp->language);
+		scanf("%s,", temp->country);
+		scanf("%s,", temp->content_rating);
+		scanf("%d,", temp->budget);
+		scanf("%d,", temp->title_year);
+		scanf("%d,", temp->actor_2_facebook_likes);
+		temp->double imdb_score);
+		temp->double aspect_ratio); //idk the letter for double
+		scanf("%d/n", temp->movie_facebook_likes); //might cause error if last characteris not newline
+		insertNode(temp);
+	}//LinkList is complete with all elements
+
+	//*all stored strings need to be trimmed when actually sorting*
+
+	int counter = 0;
+
+	Node* ptr = front;
+	while(ptr != NULL){
+		counter++;
+		ptr = ptr->next;
+	}//# of LinkList elements set
+
+	Listing[counter] collection; //not sure if this needs to be malloced
+
+	ptr = front;
+
+	for(int i = 0; i < counter; i++){
+		collection[i] = /*&*/ptr->element; //not sure if it needs & symbol
+	}
+
+	collection = MergeSort(collection);
+
+	//curtains close
 }
--- Asst0/simpleCSVsorter.h
+++ Asst0/simpleCSVsorter.h
@@ -9,28 +9,28 @@
 //Suggestion: define a struct that mirrors a record (row) of the data set
 
 typedef struct Listing{
-	char* color;
-	char* director_name;
+	char[20] color;
+	char[50] director_name;
 	int num_critic_for_reviews;
 	int duration;
 	int director_facebook_likes;
 	int actor_3_facebook_likes;
-	char* actor_2_name;
+	char[50] actor_2_name;
 	int actor_1_facebook_likes;
 	int gross;
-	char* genres;
-	char* actor_1_name;
-	char* movie_title;
+	char[200] genres;
+	char[50] actor_1_name;
+	char[100] movie_title;
 	int num_voted_users;
 	int cast_total_facebook_likes;
-	char* actor_3_name;
+	char[50] actor_3_name;
 	int facenumber_in_poster;
-	char* plot_keywords;
-	char* movie_imdb_link;
+	char[300] plot_keywords;
+	char[60] movie_imdb_link;
 	int num_user_for_reviews;
-	char* language;
-	char* country;
-	char* content_rating;
+	char[15] language;
+	char[20] country;
+	char[10] content_rating;
 	int budget;
 	int title_year;
 	int actor_2_facebook_likes;
