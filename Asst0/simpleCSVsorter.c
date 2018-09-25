b99512f96b44bedd700b3f45ea818c89b6a2611b
RBoppana <rahul32131@yahoo.com>
2018-09-25 15:45:20 -0400

    cleanse
    cleanse

--- Asst0/simpleCSVsorter.c
+++ Asst0/simpleCSVsorter.c
@@ -17,42 +17,4 @@ int main(int argc, char** argv){
 	while(!feof(stdin)){
 	  char* string = readString(stdin, 500, ','); 
 	}
-
-	/*while(){
-		Listing* temp = (Listing*)malloc(sizeof(Listing));
-		scanf("%*[^\n]\n%[^,]s,", temp->color);
-		if (temp->color == NULL) {
-			break;
-			free(temp);
-		}
-		scanf("%s,", temp->director_name);
-		scanf("%d,", temp->num_critic_for_reviews);
-		scanf("%d,", temp->duration);
-		scanf("%d,", temp->director_facebook_likes);
-		scanf("%d,", temp->actor_3_facebook_likes);
-		scanf("%s,", temp->actor_2_name);
-		scanf("%d,", temp->actor_1_facebook_likes);
-		scanf("%d,", temp->gross);
-		scanf("%s,", temp->genres);
-		scanf("%s,", temp->actor_1_name);
-		scanf("%s,", temp->movie_title);
-		scanf("%d,", temp->num_voted_users);
-		scanf("%d,", temp->cast_total_facebook_likes);
-		scanf("%s,", temp->actor_3_name);
-		scanf("%d,", temp->facenumber_in_poster);
-		scanf("%s,", temp->plot_keywords);
-		scanf("%s,", temp->movie_imdb_link);
-		scanf("%d,", temp->num_user_for_reviews);
-		scanf("%s,", temp->language);
-		scanf("%s,", temp->country);
-		scanf("%s,", temp->content_rating);
-		scanf("%d,", temp->budget);
-		scanf("%d,", temp->title_year);
-		scanf("%d,", temp->actor_2_facebook_likes);
-		scanf("%f,", temp->imdb_score);
-		scanf("%f,", temp->aspect_ratio);
-		scanf("%d", temp->movie_facebook_likes); 
-		insertNode(temp);
-	}*/
-
 }
--- Asst0/simpleCSVsorter.h
+++ Asst0/simpleCSVsorter.h
@@ -1,37 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*typedef struct Listing{
-	char color[20];
-	char director_name[50];
-	int num_critic_for_reviews;
-	int duration;
-	int director_facebook_likes;
-	int actor_3_facebook_likes;
-	char actor_2_name[50];
-	int actor_1_facebook_likes;
-	int gross;
-	char genres[200];
-	char actor_1_name[50];
-	char movie_title[100];
-	int num_voted_users;
-	int cast_total_facebook_likes;
-	char actor_3_name[50];
-	int facenumber_in_poster;
-	char plot_keywords[300];
-	char movie_imdb_link[60];
-	int num_user_for_reviews;
-	char language[15];
-	char country[20];
-	char content_rating[10];
-	int budget;
-	int title_year;
-	int actor_2_facebook_likes;
-	double imdb_score;
-	double aspect_ratio;
-	int movie_facebook_likes;
-} Listing;*/
-
 typedef union Value {
   int integer;
   double decimal;
