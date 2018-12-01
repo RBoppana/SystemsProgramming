2e40aaa5e99e612f57c8802297415cbf6e66f448
David Tian <dt474@ilab1.cs.rutgers.edu>
2018-12-01 17:41:30 -0500

    i UsE xFcE lOoK aT mE

--- Asst3/bankingClient.c
+++ Asst3/bankingClient.c
@@ -3,13 +3,45 @@
 #include <string.h>
 #include <unistd.h>
 #include <ctype.h>
-#include <dirent.h>
-#include <fcntl.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include "bankingClient.h"
+#include <netinet/in.h>
+#include <netdb.h> 
 
 int main(int argc, char** argv){
+  if (argc != 3){
+    fprintf(stderr, "Please specify the hostname and port of the server.\n");
+    return -1;
+  }
 
-	return 0;
+  int port = atoi(argv[2]);
+  if (port == 0){
+    fprintf(stderr, "Invalid port number.\n");
+    return -1;
+  }
+
+  //Socket setup
+  int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in server;
+  struct hostent *he;
+  if (socketfd < 0){
+    fprintf(stderr, "Error opening socket.\n");
+    return -1;
+  }
+  he = gethostbyname(argv[1]);
+  if (he == NULL) {
+    fprintf(stderr,"Host not found.\n");
+    exit(0);
+  }
+  server.sin_family = AF_INET;
+  memcpy(&server.sin_addr, he->h_addr_list[0], he->h_length);
+  server.sin_port = htons(port);
+
+  //Connect to server
+  while (serverfd < 0){
+    fprintf(stdout, "Connecting to %s on port %d...\n", argv[1], port);
+  }
+
+  return 0;
 }
